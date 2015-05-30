815b9dfc56d6024376d20c55e20dadbde765e020
Julien Fiaffé <julien.fiaffe@live.fr>
2015-05-30 12:13:26 +0200

    Adding Messages class

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,14 @@
+﻿namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/system messages
+        #endregion
+
+        #region Success messages
+        #endregion
+
+        #region Error messages
+        #endregion
+    }
+}
