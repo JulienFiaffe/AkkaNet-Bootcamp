77bf72d91ee158f26c291a98f1a5098503878716
Julien Fiaffé <julien.fiaffe@live.fr>
2015-05-30 12:16:53 +0200

    Adding InputSuccess message

--- src/Unit-1/DoThis/Messages.cs
+++ src/Unit-1/DoThis/Messages.cs
@@ -10,6 +10,19 @@
         #endregion
 
         #region Success messages
+        // in Messages.cs
+        /// <summary>
+        /// Base class for signalling that user input was valid.
+        /// </summary>
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
         #endregion
 
         #region Error messages
