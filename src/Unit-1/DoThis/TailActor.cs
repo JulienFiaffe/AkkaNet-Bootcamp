b348aad001593b4ea78a2f51f22f2a2338f94ac9
Julien Fiaffé <julien.fiaffe@live.fr>
2015-06-04 14:20:07 +0200

    Moving initialization of TailActor in PreStart()

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -59,15 +59,18 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
 
+        protected override void PreStart()
+        {
             // start watching file for changes
             _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
             _observer.Start();
